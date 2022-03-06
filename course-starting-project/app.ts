a58f950c9c2f41842048451564bcf99501ddde50
Ilies Paul Daniel <pauldaniel.ilies@gmail.com>
2022-03-06 09:09:08 +0200

    types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,1 +1,21 @@
-console.log('Your code goes here...');
+// basic definition of types for each input
+// types in TypeScript: number, boolean,string
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    let result = n1 + n2;
+    if (showResult) console.log(phrase + result)
+    return result;
+}
+// assing each parameter for add function
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = `Result is: `
+// end assignment
+
+
+const result = add(number1, number2, printResult, resultPhrase)
+console.log(result)
+
+// if input is not the type definition, will throw an error
+
